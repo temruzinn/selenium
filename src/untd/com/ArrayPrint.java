51b48e6636395643ff10b85b73d467bb721041ba
Rasaputhra <rpraveen@corp.untd.com>
2019-04-24 10:32:53 +0530

    git first commit

---
+++ src/untd/com/ArrayPrint.java
@@ -0,0 +1,24 @@
+package untd.com;
+public class ArrayPrint {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int intArray[];
+		intArray= new int[10];
+		
+		for (int i=0;i<intArray.length;i++){
+			intArray[i]=i+1;
+		}
+		
+		for(int i=1;i<intArray.length;i++){
+			
+			for(int j=0;j<i;j++)
+			{
+				System.out.print(intArray[j]);
+			}
+			System.out.print("\n");
+		}		
+		
+	}
+}
